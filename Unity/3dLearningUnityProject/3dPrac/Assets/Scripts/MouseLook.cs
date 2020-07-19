5714459fe043be5029f9041fe28ff0a7e3a88cb9
juneaubro <55204879+juneaubro@users.noreply.github.com>
2020-07-19 17:40:36 -0400

    basic char movement and textures

---
+++ Unity/3dLearningUnityProject/3dPrac/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSenitivity = 100f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSenitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSenitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Unity/3dLearningUnityProject/3dPrac/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Vector3 pos = Door.transform.position;
+
+        if(collision.gameObject.tag == "Door")
+        {
+
+            Debug.Log(":)");
+
+            pos.y = 2f;
+            //if (pos.y < 2f) 
+            //{
+            //    pos.y += 0.1f;
+            //}
+        }
+    }
+
+    private void OnTriggerExit(Collider asd)
+    {
+        Vector3 pos = Door.transform.position;
+
+        if(asd.gameObject.tag == "Door")
+        {
+
+            Debug.Log(":(");
+
+            if(pos.y > 0f)
+            {
+                pos.y -= 0.1f;
+            }
+        }
+    }
+}

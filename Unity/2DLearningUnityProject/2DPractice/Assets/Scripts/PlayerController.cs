fe297b7baa83caa7724d1e4b32000fe6918d4a17
juneau <juneaugawat@hotmail.com>
2020-12-15 21:11:38 -0500

    I fucked everything. Expand the box for details.
    So in this merge:
    
    - 2D Unity prac was updated for multiplayer implementation
    
    - 3D unity prac was just tampered nothing changed really
    
    - Unreal was not changed

---
+++ Unity/2DLearningUnityProject/2DPractice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private float jumpForce = 5f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.x > 0.01f)
+            Flip(1);
+
+        if (rb.velocity.x < -0.01f)
+            Flip(-1);
+
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveChar();
+    }
+
+    void Flip(float direction)
+    {
+        Vector3 flip = new Vector3(direction, transform.localScale.y, transform.localScale.z);
+
+        transform.localScale = flip;
+    }
+
+    void MoveChar()
+    {
+        // rb move pos
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // addforce impulse
+        }
+    }
+}

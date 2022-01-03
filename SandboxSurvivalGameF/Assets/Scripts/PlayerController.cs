0845a911ae12058efe63f883df323c94862750e4
juneau <juneaugawat@hotmail.com>
2022-01-03 16:34:54 -0500

    movement changes

--- SandboxSurvivalGameF/Assets/Scripts/PlayerController.cs
+++ SandboxSurvivalGameF/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     private Vector3 input;
+    private Vector3 mov;
+    private Vector3 gravity;
+    private bool jump = false;
     public bool grounded;
+    public float gravityMultiplier = 1f;
     public float jumpForce = 7f;
     public float walkSpeed = 0.1f;
 
@@ -18,11 +22,15 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate() {
         Move();
-        Jump();
+        Gravity();
+        if (jump)
+            Jump();
     }
 
     private void Update() {
         grounded = GroundCheck.isGrounded;
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            jump = true;
     }
 
     private void Move() {
@@ -30,13 +38,24 @@ public class PlayerController : MonoBehaviour {
             Input.GetAxisRaw("Horizontal") * walkSpeed,
             0,
             Input.GetAxisRaw("Vertical") * walkSpeed);
+        mov = input.x * transform.right + input.z * transform.forward;
 
-        transform.Translate(input);
+        rb.AddForce(mov);
+    }
+
+    private void Gravity() {
+        if (!grounded) {
+            gravity = new Vector3(
+                0,
+                -(Mathf.Abs(Mathf.Pow(Physics.gravity.y, 2f) * gravityMultiplier)),
+                0);
+
+            rb.AddForce(gravity);
+        }
     }
 
     private void Jump() {
-        if(Input.GetKeyDown(KeyCode.Space) && grounded) {
-            rb.velocity = Vector3.up * jumpForce;
-        }
+        rb.velocity = new Vector3(mov.x,1,mov.z) * jumpForce;
+        jump = false;
     }
 }

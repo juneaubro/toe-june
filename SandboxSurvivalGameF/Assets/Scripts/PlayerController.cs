8f8ccf47299452603efd8de55d17018415f5e7f0
juneau <juneaugawat@hotmail.com>
2022-05-18 21:54:20 -0400

    Playermovement rehaul with studio code shit

--- SandboxSurvivalGameF/Assets/Scripts/PlayerController.cs
+++ SandboxSurvivalGameF/Assets/Scripts/PlayerController.cs
@@ -1,87 +1,92 @@
 ﻿using UnityEngine;
 using Unity.Netcode;
+using System.Collections;
+using System.Collections.Generic;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : NetworkBehaviour {
-    private Rigidbody rb;
-    private Vector3 input;
-    private Vector3 mov;
-    private Vector3 gravity;
-    public static bool jump = false;
-    public static bool sprint = false;
-    public float SPEED_LIMITER = 0.02f;
-    public bool grounded;
-    public float gravityMultiplier = 55f;
-    public float jumpForce = 25f;
-    public float walkSpeed = 23f;
-    public float sprintSpeed = 100.5f;
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float slopeForce;
+    [SerializeField] private float slopeForceRayLength;
+    private CharacterController charController;
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+    private bool isJumping;
 
-    private void Start() {
-        rb = GetComponent<Rigidbody>();
-        rb.mass = 1f;
-        rb.drag = 4f;
-        rb.angularDrag = 2f;
-    }
-    private void FixedUpdate() {
-        Move();
-        Gravity();
-        if (jump)
-            Jump();
-    }
-    private void Update() {
-        grounded = GroundCheck.isGrounded;
-        if (Input.GetButton("Jump") && grounded)
-            jump = true;
-        if (Input.GetButton("Sprint") && grounded) {
-            sprint = true;
-        } else {
-            sprint = false;
-        }
-    }
-    public void Move() {
-        ///////////////////////////////////////////////////////////////////////////////////////////
-        if (!sprint) {
-            input = new Vector3(
-                Input.GetAxisRaw("Horizontal") * walkSpeed,
-                0,
-                Input.GetAxisRaw("Vertical") * walkSpeed);
-        } else {
-            input = new Vector3(
-                Input.GetAxisRaw("Horizontal") * sprintSpeed,
-                0,
-                Input.GetAxisRaw("Vertical") * sprintSpeed);
-        }
-        mov = input.x * transform.right + input.z * transform.forward;
-        rb.AddForce(mov);
-    }   
-        ////////////////////////////////////////////////////////////////////////////////////////////
-
-    private void Gravity() {
-        if (!grounded) {
-            rb.AddForce(new Vector3(0, -1, 0) * gravityMultiplier, ForceMode.Force);
-        } else {
-            rb.AddForce(new Vector3(0, rb.velocity.y * -1, 0));
-        }
-        // maybe do F = ma later
-    }
-    private void Jump() {
-        rb.AddForce(new Vector3(mov.x * SPEED_LIMITER,
-            1,
-            mov.z * SPEED_LIMITER) * jumpForce,
-            ForceMode.VelocityChange);
-        jump = false;
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
     }
 
-    /// ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxisRaw(horizontalInputName);
+        float vertInput = Input.GetAxisRaw(verticalInputName);
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        if ((vertInput != 0 || horizInput != 0) && OnSlope())
+            charController.Move(Vector3.down * charController.height / 2 * slopeForce * Time.deltaTime);
+
+        JumpInput();
+    }
+
+    private bool OnSlope()
+    {
+        if (isJumping)
+            return false;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * slopeForceRayLength))
+            if (hit.normal != Vector3.up)
+                return true;
+        return false;
+    }
+
+    private void JumpInput()
+    {
+        if(Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+
     public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
 
     public override void OnNetworkSpawn()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.mass = 1f;
-        rb.drag = 4f;
-        rb.angularDrag = 2f;
-        Move();
+        
     }
 }
